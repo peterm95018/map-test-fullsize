f5349df23b5ac2d4baeee4f79f8b9f9193e48991
Peter McMillan <peterm95018@gmail.com>
2016-07-12 15:04:55 -0700

    Added AngularJS
    Added AngularJS via yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mapFullsizeAngularApp
+ * @description
+ * # mapFullsizeAngularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mapFullsizeAngularApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mapFullsizeAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mapFullsizeAngularApp
+ */
+angular.module('mapFullsizeAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
